a0da4966e7a70ec7550203ae02d536a4eb3f803f
k-onoue <k.onoue0724@gmail.com>
2024-11-07 17:41:32 +0900

    update

--- tests/_src.py
+++ tests/_src.py
@@ -8,15 +8,17 @@ config.read(config_path)
 PROJECT_DIR = config["paths"]["project_dir"]
 sys.path.append(PROJECT_DIR)
 
-from src.covfunc import (
-    squaredExponential,
-    matern32,
-    matern52
-)
+from src.covfunc import squaredExponential, matern32, matern52
+from src.surrogates.gp import GaussianProcess, GaussianProcessMCMC
+from src.surrogates.stp import tStudentProcess, tStudentProcessMCMC
 
 
 __all__ = [
-    'squaredExponential',
-    'matern32',
-    'matern52'
+    "squaredExponential",
+    "matern32",
+    "matern52",
+    "GaussianProcess",
+    "GaussianProcessMCMC",
+    "tStudentProcess",
+    "tStudentProcessMCMC",
 ]
